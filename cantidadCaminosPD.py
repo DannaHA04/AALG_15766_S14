91e66e29b3064cc4bb8f3c28197ec690b4e79609
DannaHA04 <huamanalvarezdanna@gmail.com>
2025-06-27 20:24:44 -0500

    Caminos con Programacion Dinamica

---
+++ cantidadCaminosPD.py
@@ -0,0 +1,44 @@
+lab=[[1,1,1,1,1,1,1,1],
+     [1,1,0,1,1,1,0,1],
+     [1,1,1,1,0,1,1,1],
+     [0,1,0,1,1,0,1,1],
+     [1,1,0,1,1,1,1,1],
+     [1,1,1,0,0,1,0,1],
+     [1,0,1,1,1,0,1,1],
+     [1,1,1,1,1,1,1,1]]
+
+mem=[[-1]*len(lab[0]) for _ in range(len(lab))]
+
+def caminosPD(lab, mem, f,c):
+    global op
+    op += 1
+    if mem[f][c] != -1:
+        return mem[f][c]
+    elif f == len(lab)-1 and c == len(lab[0])-1 :
+        if lab[f][c] == 1:
+            mem[f][c] = 1
+            return mem[f][c]
+        else:
+            return 0
+    elif c == len(lab[0])-1:
+        if lab[f][c] == 1:
+            mem[f][c] = caminosPD(lab, mem, f+1,c)
+            return mem[f][c]
+        else:
+            return 0
+    elif f == len(lab)-1:
+        if lab[f][c] == 1:
+            mem[f][c] = caminosPD(lab, mem, f,c+1)
+            return mem[f][c]
+        else:
+            return 0
+    else:
+        if lab[f][c] == 1:
+            mem[f][c] = caminosPD(lab, mem, f,c+1)+caminosPD(lab, mem, f+1,c)
+            return mem[f][c]
+        else:
+            return 0
+        
+        
+op=0
+print(f"{caminosPD(lab, mem, 0,0)} caminos con {op} operaciones")
